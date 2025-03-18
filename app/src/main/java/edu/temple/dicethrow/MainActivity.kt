211dddd2a4beb44adf65486f159ab43439756d6b
tun93460 <ldgraham2002@gmail.com>
2025-03-18 11:37:53 -0400

    Created an onclick listener for the rollDiceButton that uses the fragment manager to get an instance of the DieFragment and execute the throwDie method

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,14 +1,25 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.rollDiceButton).apply{
+            setOnClickListener{
+                (supportFragmentManager
+                    .findFragmentById(R.id.fragmentContainerView) as DieFragment).throwDie()
+
+            }
+        }
     }
 }
